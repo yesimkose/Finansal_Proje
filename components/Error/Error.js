efce013040201738329db10b77e118800e6d8ea4
yesimkose <63107139+yesimkose@users.noreply.github.com>
2021-12-15 23:58:02 +0300

    Delete components directory

--- components/Error/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LottieView from 'lottie-react-native';
-import { ActivityIndicator } from 'react-native';
-
-function Error() {
-    return <ActivityIndicator source={require('../../assets/error.json')} autoPlay/>
-}
-export default Error
-
--- components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LottieView from 'lottie-react-native';
-import { ActivityIndicator } from 'react-native';
-
-function Loading() {
-    return <ActivityIndicator source={require('../../assets/loading.json')} autoPlay/>
-}
-export default Loading
