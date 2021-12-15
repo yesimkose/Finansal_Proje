a951514ad8489454af09f6da7fb80e82618364de
yesimkose <63107139+yesimkose@users.noreply.github.com>
2021-12-15 23:34:47 +0300

    Add files via upload

--- components/Error/Error.js
+++ components/Error/Error.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import LottieView from 'lottie-react-native';
+import { ActivityIndicator } from 'react-native';
 
 function Error() {
-    return <LottieView source={require('../../assets/error.json')} autoPlay/>
+    return <ActivityIndicator source={require('../../assets/error.json')} autoPlay/>
 }
 export default Error
 
--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import LottieView from 'lottie-react-native';
+import { ActivityIndicator } from 'react-native';
 
 function Loading() {
-    return <LottieView source={require('../../assets/loading.json')} autoPlay/>
+    return <ActivityIndicator source={require('../../assets/loading.json')} autoPlay/>
 }
 export default Loading
