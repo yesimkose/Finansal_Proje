f2e32ae6b167b8d2a9ea3e0dae9975c7763328cd
yesimkose <63107139+yesimkose@users.noreply.github.com>
2021-12-15 23:38:23 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+import Categories from './pages/Categories/Categories';
+import Detail from './pages/Detail/Detail';
+import Meals from './pages/Meals/Meals';
+
+
+export default function App() {
+
+  const Stack=createStackNavigator();
+
+  return(
+<NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen 
+    name="CatogoriesPage" 
+    component={Categories}
+    options={{
+      title:'Categories',
+      headerTitleAlign:'center',
+      headerTintColor:'orange',
+      headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize:26,
+      fontFamily: "Cochin",
+    }}}/>
+    <Stack.Screen 
+    name="MealsPage" 
+    component={Meals}
+    options={{
+      title:'Meals',
+      headerTitleAlign:'center',
+      headerTintColor:'orange',
+      headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize:26,
+      fontFamily: "Cochin",
+    }}}
+    />
+    <Stack.Screen
+     name="DetailPage" 
+     component={Detail}
+     options={{
+      title:'Detail',
+      headerTitleAlign:'center',
+      headerTintColor:'orange',
+      headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize:26,
+      fontFamily: "Cochin",
+    }}}/>
+  </Stack.Navigator>
+</NavigationContainer>
+  )
+  
+}
+
