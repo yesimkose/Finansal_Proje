4c99be75db04fe4bca981788e38d9275f3af91ac
yesimkose <63107139+yesimkose@users.noreply.github.com>
2021-12-15 23:37:08 +0300

    Delete src/components directory

--- src/components/Button/FovoriteButton.styles.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions, StyleSheet } from "react-native";
-
-export default StyleSheet.create({
- button:{
-   backgroundColor:"#FF6B6B",
-   fontWeight:"bold",
- }
-})
--- src/components/Loading/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { ActivityIndicator } from 'react-native';
-
-function Loading() {
-    return <ActivityIndicator/>
-}
-export default Loading
